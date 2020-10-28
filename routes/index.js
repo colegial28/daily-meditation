37e370bc50476ec8bdd8760d522262730cca418e
colegial28 <colegial28@users.noreply.github.com>
2020-10-28 13:29:50 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,16 @@ const indexController= require('../controllers/index')
 
 router.get('/', indexController.getIndex);
 
+router.get('/register',indexController.getRegister);
+
+router.post('/register',indexController.postRegister)
+
+
+router.get('/login',indexController.getLogin);
+
+router.post('/login', indexController.postLogin);
+
+router.post('/logout',indexController.postLogout)
+
 
 module.exports = router;
